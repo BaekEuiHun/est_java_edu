d615f031cf2cd3483ce7d2e8533d54fa55a2c870
BaekEuiHun <euihunbaek@gmail.com>
2024-05-31 13:40:07 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,6 @@
 public class Test {
   public static void main(String[] args) {
-      String firstName = "eui hun";
-      String secondName = "baek";
-      String phone = "iphone";
-      String status = "good";
-      System.out.println("나는 " + secondName + " " + firstName + "입니다");
-      System.out.println("나의 폰은 : " + phone + "이고 " + "나의 기분은 : " + status + "입니다");
-      String b = "estsoft";
-      System.out.println(b.contains("soft"));
+     int [] arr = new int[10];
+    System.out.println(arr[0]);
   }
 }
