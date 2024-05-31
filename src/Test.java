872ac4625a0911120f85553dc5c7f6a05163c5f5
BaekEuiHun <euihunbaek@gmail.com>
2024-05-31 13:38:20 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,12 @@
 public class Test {
-    public static void main(String[] args) {
-        int a = 10;
-        System.out.println(a);
-        int b = 10;
-        System.out.println(b);
-    }
+  public static void main(String[] args) {
+      String firstName = "eui hun";
+      String secondName = "baek";
+      String phone = "iphone";
+      String status = "good";
+      System.out.println("나는 " + secondName + " " + firstName + "입니다");
+      System.out.println("나의 폰은 : " + phone + "이고 " + "나의 기분은 : " + status + "입니다");
+      String b = "estsoft";
+      System.out.println(b.contains("soft"));
+  }
 }
