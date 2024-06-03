14e03134cad13c8196cf41a2ec2ea1d2e51f14ed
BaekEuiHun <euihunbaek@gmail.com>
2024-06-03 13:47:21 +0900

    update

--- src/If/IfEx3.java
+++ src/If/IfEx3.java
@@ -6,9 +6,9 @@ public class IfEx3 {
         int b = 20;
         int c = 30;
 
-        if(a>b && a>c){
+        if(a>=b && a>=c){
       System.out.println(a);
-        } else if(b>a && b>c){
+        } else if(b>=a && b>=c){
             System.out.println(b);
         } else {
             System.out.println(c);
