0b6ab4657532002066fba2bf43d5ced404d41f56
BaekEuiHun <euihunbaek@gmail.com>
2024-06-03 13:45:50 +0900

    update

--- src/If/IfEx2.java
+++ src/If/IfEx2.java
@@ -3,7 +3,7 @@ package If;
 public class IfEx2 {
     public static void main(String[] args){
         int year = 2023;
-        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+        if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
         }
         System.out.println("윤년입니다");
     }
