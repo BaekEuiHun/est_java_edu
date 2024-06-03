cc03ed9ebee68175d497ad24009dae858057a18e
BaekEuiHun <euihunbaek@gmail.com>
2024-06-03 15:33:16 +0900

    update

---
+++ src/Switch/Switch7.java
@@ -0,0 +1,24 @@
+package Switch;
+
+public class Switch7 {
+    public static void main(String[] args){
+
+        String language = "Java";
+        switch (language) {
+            case "Java":
+                System.out.println("Java programming language");
+                break;
+            case "Python":
+                System.out.println("Python programming language");
+                break;
+            case "C++":
+                System.out.println("C++ programming language");
+                break;
+            case "JavaScript":
+                System.out.println("JavaScript programming language");
+                break;
+            default:
+        System.out.println("다시 입력하세요");
+    }
+    }
+}
