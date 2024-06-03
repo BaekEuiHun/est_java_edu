d8bccd90e4da2ec6ee1c1098cc749a4acd9a2eac
BaekEuiHun <euihunbaek@gmail.com>
2024-06-03 15:11:00 +0900

    update

---
+++ src/switch1/Switch2.java
@@ -0,0 +1,33 @@
+package switch1;
+
+public class Switch2 {
+  public static void main(String[] args) {
+    String day = "일";
+
+    switch (day) {
+      case "월":
+        System.out.println("책 읽기");
+        break;
+      case "화":
+        System.out.println("잠자기");
+        break;
+      case "수":
+        System.out.println("영화보기");
+        break;
+      case "목":
+        System.out.println("운동하기");
+        break;
+      case "금":
+        System.out.println("놀기");
+        break;
+      case "토":
+        System.out.println("공부하기");
+        break;
+      case "일":
+        System.out.println("놀러가기");
+        break;
+      default:
+        System.out.println("다시 입력하세요");
+    }
+  }
+}
