531138c53d00054f60bd26d8915886da161b4ce9
BaekEuiHun <euihunbaek@gmail.com>
2024-06-03 15:08:49 +0900

    update

---
+++ src/switch1/Switch.java
@@ -0,0 +1,20 @@
+package switch1;
+
+public class Switch {
+  public static void main(String[] args) {
+
+    int data = 10;
+    switch (data) {
+      case 1:
+        System.out.println("1입니다");
+        break;
+      case 2:
+        System.out.println("2입니다");
+        break;
+        case 10:
+        System.out.println("10입니다");
+        default:
+        System.out.println("다시 입력하세요");
+    }
+  }
+    }
