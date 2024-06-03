8e9519ac3b8c72cedb709220a83e9aa03a6c623f
BaekEuiHun <euihunbaek@gmail.com>
2024-06-03 11:29:59 +0900

    init

---
+++ src/If.java
@@ -0,0 +1,12 @@
+public class If {
+    public static void main(String[] args){
+        int score = 95;
+        char grade;
+        if(score>90) {
+            grade = 'A';
+        } else {
+            grade = 'B';
+        }
+    System.out.println("당신의 학점은 : " + grade);
+    }
+}
