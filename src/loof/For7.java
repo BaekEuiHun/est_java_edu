c26e638eb81b7e91a7b07130a0186fb7bd2703fe
BaekEuiHun <euihunbaek@gmail.com>
2024-06-04 14:24:40 +0900

    update

---
+++ src/loof/For7.java
@@ -0,0 +1,12 @@
+package loof;
+
+public class For7 {
+  public static void main(String[] args) {
+
+    for (int i = 19; i > 0; i--) {
+      for (int k = 19; k > 0; k--) {
+        System.out.println(i + "x" + k + "=" + i * k);
+      }
+    }
+  }
+}
