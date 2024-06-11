ec767c3052d46abc5b8aeb4ae7a8395f259b7052
BaekEuiHun <euihunbaek@gmail.com>
2024-06-11 10:24:55 +0900

    update

--- src/scanner/Scanner5.java
+++ src/scanner/Scanner5.java
@@ -8,22 +8,22 @@ public class Scanner5 {
     int choice;
     int num1;
     int num2;
-
+    System.out.println("원하는 기능을 선택하세요");
     System.out.println("1.덧셈 / 2.뺄셈 / 3.곱셈 / 4.나눗셈 / 0.종료");
     choice = sc.nextInt();
     switch (choice) {
       case 1:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요");
         break;
 
       case 2:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요");
         break;
       case 3:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요");
         break;
       case 4:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요");
         break;
       case 0:
         System.out.println("종료합니다");
@@ -32,7 +32,7 @@ public class Scanner5 {
         System.out.println("올바른 입력이 아닙니다");
     }
     num1 = sc.nextInt();
-    System.out.println("두번째 값을 입력하세요");
+    System.out.println("두번째 값을 입력하고 엔터를 누르세요");
     num2 = sc.nextInt();
     switch (choice) {
       case 1:
@@ -54,17 +54,17 @@ public class Scanner5 {
     choice = sc.nextInt();
     switch (choice) {
       case 1:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println(("첫번째 값을 입력하고 엔터를 누르세요"));
         break;
 
       case 2:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println(("첫번째 값을 입력하고 엔터를 누르세요"));
         break;
       case 3:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요");
         break;
       case 4:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요");
         break;
       case 0:
         System.out.println("종료합니다");
@@ -77,17 +77,17 @@ public class Scanner5 {
     choice = sc.nextInt();
     switch (choice) {
       case 1:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요");
         break;
 
       case 2:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요");
         break;
       case 3:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요");
         break;
       case 4:
-        System.out.println("첫번째 값을 입력하세요");
+        System.out.println("첫번째 값을 입력하고 엔터를 누르세요");
         break;
       case 0:
         System.out.println("종료합니다");
@@ -96,7 +96,7 @@ public class Scanner5 {
         System.out.println("올바른 입력이 아닙니다");
     }
     num1 = sc.nextInt();
-    System.out.println("두번째 값을 입력하세요");
+    System.out.println("두번째 값을 입력하고 엔터를 누르세요");
     num2 = sc.nextInt();
     switch (choice) {
       case 1:
