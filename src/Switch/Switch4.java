918e7be386a7ef45096870ab7277ffdc10cccec9
BaekEuiHun <euihunbaek@gmail.com>
2024-06-14 21:05:25 +0900

    Update Switch4.java

--- src/Switch/Switch4.java
+++ src/Switch/Switch4.java
@@ -6,7 +6,7 @@ public class Switch4 {
         int num1 = 3, num2 = 1;
         switch (operator) {
             case '+':
-                System.out.println(num1 + num2) ;
+                System.out.println(num1 + num2);
                 break;
             case '-':
                 System.out.println(num1 - num2);
