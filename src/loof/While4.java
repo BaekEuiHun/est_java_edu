15b0d8dac10a034665a0cea6177dcb9031f1f382
BaekEuiHun <euihunbaek@gmail.com>
2024-06-16 23:33:06 +0900

    Update While4.java

--- src/loof/While4.java
+++ src/loof/While4.java
@@ -4,7 +4,7 @@ public class While4 {
   public static void main(String[] args) {
 
     int i = 0;
-    while (i <= 300){
+    while (i <= 300) {
       if (i % 3 == 0) {
         System.out.println(i);
       }
